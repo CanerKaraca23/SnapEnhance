079c35a34a9deffea55bf3718dfa3fbc43e9c449
Caner Karaca <37447503+CanerKaraca23@users.noreply.github.com>
2024-10-02 21:06:38 +0000

    Update

--- native/build.gradle.kts
+++ native/build.gradle.kts
@@ -9,8 +9,6 @@ val nativeName = rootProject.ext.get("buildHash")
 android {
     namespace = rootProject.ext["applicationId"].toString() + ".nativelib"
     compileSdk = 34
-
-    buildToolsVersion = "34.0.0"
     ndkVersion = "27.1.12297006"
 
     buildFeatures {
